5c297631dc2cf3b4ffb5837e05dbf7e0d7daf252
Hamid Zafar <129169899+i-HamidZafar@users.noreply.github.com>
2024-08-25 19:28:24 +0500

    Week6Lecture till CSV

--- Week6Lecture.py
+++ Week6Lecture.py
@@ -44,7 +44,7 @@
       for line in file:
           print("hello,", line.rstrip())
 
-  # displaying the prompt by names
+# displaying the prompt by names
   names = []
   with open("names.txt") as file:
       for line in file:
@@ -59,29 +59,29 @@ _________________________________________
        
 #students.py
 # reading 2D file
-with open("students.csv") as file:
-    students = []
-    for line in file:
-        row = line.rstrip().split(",")
-        print(f"{row[0]} is in {row[1]}")
+  with open("students.csv") as file:
+      students = []
+      for line in file:
+          row = line.rstrip().split(",")
+          print(f"{row[0]} is in {row[1]}")
       
 # less cryptic and unpacking the list 
-with open("students.csv") as file:
-    students = []
-    for line in file:
-        name, house = line.rstrip().split(",")
-        print(f"{name} is in {house}")
+  with open("students.csv") as file:
+      students = []
+      for line in file:
+          name, house = line.rstrip().split(",")
+          print(f"{name} is in {house}")
 
 # sorting by name, not well designed
 
-students = []
-with open("students.csv") as file:
-    for line in file:
-        name, house = line.rstrip().split(",")
-        students.append(f"{name} is in {house}")
-
-for student in sorted(students):
-    print(student)
+  students = []
+  with open("students.csv") as file:
+      for line in file:
+          name, house = line.rstrip().split(",")
+          students.append(f"{name} is in {house}")
+  
+  for student in sorted(students):
+      print(student)
 
 # using dict
 
@@ -95,37 +95,101 @@ for student in sorted(students):
       print(f"{student['name']} is in {student['house']}")
 
 # improving it further [sorting by a "key" in a dictionary]
-def main():
-    students= []
-    with open("students.csv") as file:
-        for line in file:
-            name, house = line.rstrip().split(",")
-            student = {"name": name , "house": house}
-            students.append(student)
-    for student in sorted(students, key= get_name, reverse=True):
-        print(f"{student['name']} is in {student['house']}")
+  def main():
+      students= []
+      with open("students.csv") as file:
+          for line in file:
+              name, house = line.rstrip().split(",")
+              student = {"name": name , "house": house}
+              students.append(student)
+      for student in sorted(students, key= get_name, reverse=True):
+          print(f"{student['name']} is in {student['house']}")
+  
+  def get_name(student):
+      return student["name"]
+  def get_house(student):
+      return student["house"]
+  
+  if __name__ == "__main__":
+      main()
 
-def get_name(student):
-    return student["name"]
-def get_house(student):
-    return student["house"]
+# using lambda function [better]
 
-if __name__ == "__main__":
-    main()
+  def main():
+      students = []
+  
+      with open("students.csv") as file:
+          for line in file:
+              name , house = line.rstrip().split(",")
+              student = {"name": name, "house": house}
+              students.append(student)
+      for student in sorted(students, key= lambda student: student["name"], reverse=True):
+          print(f"{student['name']} is in {student['house']}")
+  
+  if __name__ =="__main__":
+      main()
 
-# using lambda function
 
-def main():
-    students = []
 
-    with open("students.csv") as file:
-        for line in file:
-            name , house = line.rstrip().split(",")
-            student = {"name": name, "house": house}
-            students.append(student)
-    for student in sorted(students, key= lambda student: student["name"], reverse=True):
-        print(f"{student['name']} is in {student['house']}")
+#uisng CSV MODULE to handle seperation, parsing of values within the file more conventiently with more effective reads
 
-if __name__ =="__main__":
+  # using csv reader
+  import csv
+  def main():
+      students = []
+  
+      with open("students.csv") as file:
+          reader = csv.reader(file)
+          for name, home in reader:
+              students.append({"name": name, "home": home})
+  
+  
+      for student in sorted(students, key= lambda student: student["name"], reverse=True):
+          print(f"{student['name']} is from {student['home']}")
+  
+  if __name__ =="__main__":
+      main()
+
+  #using DictReader [you can explicitly bake the information about what each column name is to use it] more robust code
+  
+  import csv
+  def main():
+      students = []
+  
+      with open("students.csv") as file:
+          reader = csv.DictReader(file)
+          for row in reader:
+              students.append(row)
+  
+  
+      for student in sorted(students, key= lambda student: student["name"], reverse=True):
+          print(f"{student['name']} is from {student['home']}")
+  
+  if __name__ =="__main__":
+      main()
+
+# CSV WRITER
+  # using simple writer
+  import csv
+  
+  def main():
+      name = input("what's your name? ")
+      home = input("what's your home? ")
+      with open("students.csv", "a") as file:
+          writer = csv.writer(file)
+          writer.writerow([name,home])
+  if __name__ == "__main__":
+      main()
+
+  # using DictWriter
+  import csv
+  
+  def main():
+    name = input("what's your name? ")
+    home = input("what's your home? ")
+    with open("students.csv", "a", newline= "") as file:
+        writer = csv.DictWriter(file, fieldnames=["name", "home"])#coloumns ordering in the file [fieldnames]
+        writer.writerow({"name": name, "home": home})
+  if __name__ == "__main__":
     main()
 
